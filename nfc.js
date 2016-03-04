4dbd8db4145fafc6e88b6be1e90145e5b22c9e2d
Johan Andersson <johan-spam@andersson.net>
2016-03-04 20:31:52 +0100

    Add some hackish server-side throttling and use named events

--- nfc.js
+++ nfc.js
@@ -10,30 +10,34 @@ var middleware = function (req, res, next) {
         });
     };
 
-    res.sseSend = function(data) {
-        res.write('data: ' + JSON.stringify(data) + '\n\n');
+    res.sseSend = function(eventName, data) {
+        res.write( 'event: ' + eventName + '\ndata: ' + JSON.stringify(data) + '\n\n');
     };
 
     next();
 }
 
 var init = function(app) {
-    var tagData = { uid: null };
     var connections = [];
+    var lastUid;
 
     app.use(middleware);
     app.get('/events', function(req, res) {
         res.sseSetup();
-        res.sseSend(tagData);
+        res.sseSend( 'ping', new Date() );
         connections.push(res);
     });
 
     n.on('uid', function(uid) {
         console.log('UID:', uid);
-        tagData.uid = uid;
-        for(var i = 0; i < connections.length; i++) {
-            connections[i].sseSend(tagData);
+        if( lastUid === uid ) {
+            return;
         }
+        for(var i = 0; i < connections.length; i++) {
+            connections[i].sseSend( 'nfcread', { uid: uid });
+        }
+        lastUid = uid;
+        setTimeout(function() { lastUid = null; }, 5000);
     });
 
     n.start();
